71458d39684d19aef679895508e9da5caa24e03f
JWMB <jonas.beckeman@gmail.com>
2025-03-25 18:41:54 +0100

    svgNumberButton

---
+++ src/components/svgNumberButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let { hilite, x, y, size, onclick, id }: {
+        hilite?: boolean,
+        x?: number, y?: number,
+        size: number,
+        onclick: (id: string) => void,
+        id: string
+    } = $props();
+
+    let hover = $state(false);
+</script>
+
+<g transform="translate({x},{y})" >
+    <rect width="{size*2}%" height="{size*2}%" rx="{size/5}%"
+        onmouseenter={() => hover = true}
+        onmouseleave={() => hover = false}
+        onclick={() => onclick(id)}
+        fill={hover ? "red" : (hilite ? "cyan" : "blue")} stroke="black" stroke-width="1"
+        />
+    <text fill="white" stroke="black" font-family="Arial, Helvetica, sans-serif" font-size="0.7em" stroke-width={0.2} x={size*0.7} y={size*1.3}>{id}</text>
+</g>
--- src/routes/training/activity/wmView.svelte
+++ src/routes/training/activity/wmView.svelte
@@ -2,6 +2,7 @@
 	import { type ItemLayoutFunctions, type WMController } from "$lib/controllers/wmController";
 	import { onMount, type Component } from "svelte";
 	import SvgCircleButton from "../../../components/svgCircleButton.svelte";
+	import SvgNumberButton from "../../../components/svgNumberButton.svelte";
 
     let { controller }: {
         controller: WMController | null,
@@ -48,16 +49,15 @@
   		component: Component<any>;
 		[key: string]: any; // for props
 	};
-	// const components: ComponentAndProps[] = $state([]);
 
     console.log("ASA");
     const components = $derived(items.map(item => ({
-        component: SvgCircleButton,
+        component: SvgCircleButton, //SvgCircleButton SvgNumberButton
         x: layout?.location(item, time).x,
         y: layout?.location(item, time).y,
         size: layout?.size(item, time) || 5,
         id: item.id.toString(),
-        // onclick: onClick(item),
+        // not possible, state change... onclick: onClick(item),
         hilite: item.hilite
      } as ComponentAndProps)));
 
@@ -75,12 +75,6 @@
     <div>{components.length}</div>
     <div id={time.toString()}>
         <svg viewBox="0 0 100 100" height="300px" style="filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7))">
-        <!-- {#each items as item}
-            <SvgCircleButton id={item.id.toString()} x={layout?.location(item, time).x} y={layout?.location(item, time).y} size={layout?.size(item, time) || 5}
-                hilite={item.hilite}
-                onclick={() => onClick(item)}>
-            </SvgCircleButton>
-        {/each} -->
         {#each components as c}
             <c.component {...c} onclick={() => onClick((c as any).id)}/>
         {/each}

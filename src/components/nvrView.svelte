ed7b666d6491129431e3ec65f90d4269681c2de1
JWMB <jonas.beckeman@gmail.com>
2025-04-04 17:29:18 +0200

    setDragTargets

--- src/components/nvrView.svelte
+++ src/components/nvrView.svelte
@@ -11,6 +11,7 @@
 
     let questionCards: Card[] = $state([]);
     let answerCards: Card[] = $state([]);
+    let emptySlots: {x: number, y: number}[] = $state([]);
 
     let answer: string = $state("");
 
@@ -20,11 +21,13 @@
             return;
         }
 
-        // typedController().signals.clear.add(() => {
-        //     alternatives = [];
-        // });
+        typedController().signals.setDragTargets.add(arg => {
+            emptySlots = arg.items;
+        });
+
         typedController().signals.addItems.add(arg => {
             // console.log("addItem", arg);
+            console.log("asd", arg.items.filter(o => o.type == "Q").map(o => ({ x: o.x, y: o.y})));
             for (let item of arg.items) {
                 const card: Card = { id: parseInt(item.id, 10), x: item.x, y: item.y, svg: item.text || "" };
                 if (item.type == "Q") {
@@ -34,13 +37,11 @@
                 }
             }
         });
-        // typedController().signals.enable.add(arg => enabled = arg.value);
     });
 
     // const respond = (answer: string) => typedController().respond(answer);
     // const onKeydown = (key: string) => {
     //     if (key === "Enter") {
-    //         // console.log("AA", answer);
     //         respond(answer);
     //     }
     // }
@@ -51,6 +52,10 @@
         {#each questionCards as card}
         <div style="outline: solid; box-shadow: 5px 5px 5px gray; position: absolute; left:{card.x * 120}px; top:{100 + card.y * 120}px;">
             {@html card.svg}
+        </div>
+        {/each}
+        {#each emptySlots as card}
+        <div style="outline: solid; position: absolute; width:100px; height:100px; left:{card.x * 120}px; top:{100 + card.y * 120}px;">
         </div>
         {/each}
     </div>
--- src/lib/controllers/nvrController.ts
+++ src/lib/controllers/nvrController.ts
@@ -1,15 +1,24 @@
 import type { ApiWrapper } from "$lib/apiWrapper";
+import { SignalX1, type SignalX1Public } from "$lib/signals";
 import { GameSignalsBase, type GameController, type GameSignalsPublic } from "./gameController";
 
-// export interface GameSignalsPublicNVR extends GameSignalsPublic {
-//     get clear(): SignalX0Public;
-// }
-// export class GameSignalsQnA extends GameSignalsBase implements GameSignalsPublicNVR {
-//     _clear = new SignalX0();
-//     get clear() { return this._clear.consumer; };
-// }
+export interface GameSignalsPublicNVR extends GameSignalsPublic {
+    get setDragTargets(): SignalX1Public<DragTargetsArgs>;
+}
+
+export class GameSignalsNVR extends GameSignalsBase implements GameSignalsPublicNVR {
+    _setDragTargets = new SignalX1<DragTargetsArgs>();
+    get setDragTargets() { return this._setDragTargets.consumer; };
+}
+
+export type DragTargetsArgs = { items: {x: number, y: number}[]};
 
 export class NVRController implements GameController {
+    private _signals = new GameSignalsNVR();
+    get signals(): GameSignalsPublicNVR { return this._signals; }
+    // protected _signals = new GameSignalsBase();
+    // get signals(): GameSignalsPublic { return this._signals; }
+
     constructor(protected api: ApiWrapper) { }
 
     async init() {
@@ -23,15 +32,14 @@ export class NVRController implements GameController {
         if (!stimSol) {
             throw new Error("No stimulus");
         }
-        // console.log("stimSol.stimuli", stimSol.stimuli);
-        const all = stimSol.stimuli.questionCards.map((o: any) => ({ item: o, type: "Q"}))
+
+        this._signals._setDragTargets.dispatch({ items: stimSol.stimuli.emptySlots });
+
+        const allCards = stimSol.stimuli.questionCards.map((o: any) => ({ item: o, type: "Q"}))
             .concat(stimSol.stimuli.answerCards.map((o: any) => ({ item: o, type: "A"})));
         this._signals._addItems.dispatch({items:
-            all.map((o: any) => ({ id: "", x: o.item.location.x, y: o.item.location.y, text: o.item.svg, type: o.type }))});
+            allCards.map((o: any) => ({ id: "", x: o.item.location.x, y: o.item.location.y, text: o.item.svg, type: o.type }))});
 
         return true;
     }
-
-    protected _signals = new GameSignalsBase();
-    get signals(): GameSignalsPublic { return this._signals; }
 }
